19046b196650494fc526198fce43f6c46fd55ef6
Álisson Henrique <alissonhmb@gmail.com>
2023-03-28 15:38:17 -0300

    create source in create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
+  after_save_commit :create_source
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :transactions
   has_many :sources
+
+  validates :name, presence: true
+
+  private
+
+  def create_source
+    Source.create(name: 'Banco', user_id: id)
+    Source.create(name: 'Dinheiro', user_id: id)
+  end
 end
